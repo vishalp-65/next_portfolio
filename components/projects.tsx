bd3f09ceff9e1503e0a648a96f2b723ddf68555f
vishalp-65 <vishalpanchal1828vp@gmail.com>
2024-09-10 23:26:05 +0530

    Change projects view UI

--- components/projects.tsx
+++ components/projects.tsx
@@ -9,10 +9,34 @@ import {
     MdKeyboardDoubleArrowDown,
     MdKeyboardDoubleArrowUp,
 } from "react-icons/md";
+import {
+    Carousel,
+    CarouselApi,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "./ui/carousel";
 
 export default function Projects() {
     const { ref } = useSectionInView("Projects", 0.5);
     const [maxProject, setMaxProject] = useState<number>(3);
+    const [api, setApi] = React.useState<CarouselApi>();
+    const [current, setCurrent] = React.useState(0);
+    const [count, setCount] = React.useState(0);
+
+    React.useEffect(() => {
+        if (!api) {
+            return;
+        }
+
+        setCount(api.scrollSnapList().length);
+        setCurrent(api.selectedScrollSnap() + 1);
+
+        api.on("select", () => {
+            setCurrent(api.selectedScrollSnap() + 1);
+        });
+    }, [api]);
 
     useEffect(() => {}, [maxProject]);
     return (
@@ -22,26 +46,43 @@ export default function Projects() {
             className="scroll-mt-28 mb-28 flex flex-col items-center"
         >
             <SectionHeading>My projects</SectionHeading>
-            <div className="flex flex-col items-center gap-3 justify-center">
-                {projectsData.map(
-                    (project: any, index) =>
-                        maxProject > index && (
-                            <React.Fragment key={index}>
-                                <Project
-                                    title={project.title}
-                                    description={project.description}
-                                    tags={project.tags}
-                                    gitURL={project.gitURL}
-                                    liveDemo={project.liveDemo}
-                                    details={project.details}
-                                    date={project.date}
-                                    imageUrl={project.imageUrl}
-                                />
-                            </React.Fragment>
-                        )
-                )}
+            <div className="flex flex-col items-center gap-3 justify-center rounded-md">
+                <Carousel
+                    setApi={setApi}
+                    className="flex items-center justify-center w-screen md:max-w-[50rem]"
+                >
+                    <CarouselContent>
+                        {projectsData.map((project: any, index) => (
+                            <CarouselItem
+                                key={index}
+                                className="flex items-center justify-center"
+                            >
+                                <React.Fragment key={index}>
+                                    <Project
+                                        title={project.title}
+                                        description={project.description}
+                                        tags={project.tags}
+                                        gitURL={project.gitURL}
+                                        liveDemo={project.liveDemo}
+                                        details={project.details}
+                                        date={project.date}
+                                        imageUrl={project.imageUrl}
+                                    />
+                                </React.Fragment>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious
+                        size="sm"
+                        className="hidden md:block bg-transparent"
+                    />
+                    <CarouselNext className="hidden md:block bg-transparent" />
+                </Carousel>
+                <div className="text-center text-sm text-muted-foreground">
+                    Project {current} of {count}
+                </div>
             </div>
-            {maxProject >= projectsData.length ? (
+            {/* {maxProject >= projectsData.length ? (
                 <button
                     onClick={() => setMaxProject(3)}
                     className="flex items-center gap-1 mt-8 bg-black/[0.7] p-3 text-[0.7rem] uppercase tracking-wider
@@ -59,7 +100,7 @@ export default function Projects() {
                     Show more
                     <MdKeyboardDoubleArrowDown className="w-4 h-4" />
                 </button>
-            )}
+            )} */}
         </section>
     );
 }
